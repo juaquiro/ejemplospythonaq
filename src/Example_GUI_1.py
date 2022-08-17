c7db337ca51045f7bd137ae28d983011634f2c09
aqiot <aq@iot.es>
2022-08-17 16:24:05 -0400

    documentado la interaccion entre la QApplication y los widgets

--- src/Example_GUI_1.py
+++ src/Example_GUI_1.py
@@ -13,10 +13,10 @@ from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMe
 # In Python however, any function can be a slot.
 # For this reason, the distinction between slots and "normal" functions has little relevance in Python.
 # This is simply a function that gets called when a signal occurs
-def on_button_clicked():
+def on_button_top_clicked():
     """!
-    @brief example of
-    @see testFPAUtils.py for unit tests
+    @brief slot example
+    @see
     @author AQ 2021
     """
     alert = QMessageBox()
@@ -24,11 +24,39 @@ def on_button_clicked():
     alert.exec()
 
 
-# we create a QApplication with the command:
+# slot example (callback function)
+def on_button_bottom_clicked():
+    """!
+    @brief slot example get running QApplication and quit
+    @see
+    @author AQ 2021
+    """
+
+    # get the instance of the running QApplication and quit
+    running_app = QApplication.instance()
+    running_app.quit()
+
+
+# we create a QApplication
 # This is a requirement of Qt: Every GUI app must have exactly one instance of QApplication.
+# you can capture the QApplication instance where your PyQt programs is running using
+# app=QApplication.instance()
+#
+# with the app instance you can get access to the running windows, widgets, desktop, ....
+# app.allWindows()
+# app.allWidgets()
+# app.applicationDirPath(): C:/Python/Python38
+# app.applicationFilePath(): C:/Python/Python38/python.exe
+# app.desktop().size():  PyQt5.QtCore.QSize(3840, 2160)
+# app.desktop().screenGeometry(0): PyQt5.QtCore.QRect(0, 0, 3840, 2160)
+
 # Many parts of Qt don't work until you have executed the line.
 # You will therefore need it in virtually every (Py)Qt app you write.
+# NOTE: A variable created in the main body of the Python code is a global variable and belongs to the global scope.
+# Global variables are available from within any scope, global and local.
+# therefore app is global see https://www.w3schools.com/python/python_scope.asp
 app = QApplication([])
+
 
 # Built-in styles
 # The coarsest way to change the appearance of your application is to set the global Style.
@@ -60,7 +88,8 @@ window.setLayout(layout)
 # signal connection to a slot
 # QPushButton.clicked is a signal. connect(...) lets us install the slot on it.
 # the connected slot gets called when the signal occurs
-buttonTop.clicked.connect(on_button_clicked)
+buttonTop.clicked.connect(on_button_top_clicked)
+buttonBottom.clicked.connect(on_button_bottom_clicked)
 
 # tell Qt to show the window widget on the screen:
 window.show()
